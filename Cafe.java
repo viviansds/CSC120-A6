49cfdc21db1d7e2af25619adb2b05d18781cf1d7
Vivian Wei <vwei@smith.edu>
2022-10-26 23:57:45 -0400

    Finish Cafe and House inheritance

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,45 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building{
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+
+    public Cafe(String name,String address,int nFloors) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = 100;
+        this.nSugarPackets = 100;
+        this.nCreams = 100;
+        this.nCups =100;   
     }
-    
+    // is it okay to change parameters name?
+    public void sellCoffee(int size, int sugar, int cream){
+        if (nCoffeeOunces>=size && nSugarPackets>=nSugarPackets && nCreams>=nCreams && nCups>=1) {
+            this.nCoffeeOunces -= size;
+            this.nSugarPackets -= sugar;
+            this.nCreams -= cream;
+            this.nCups -= 1;
+            System.out.println("remaining inventory: "+ nCoffeeOunces+" ounces of cofee,"+nSugarPackets+" sugar packets,"+nCreams +" number of creams, and "+nCups+"cups");
+        }else{
+            System.out.println("low in stock, restocking");
+            restock(100, 100,100,100);
+            // after restocking, should it sell cofeee again?
+        }
+    }
+      
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
+        this.nCups += nCups;
+    }
+
     public static void main(String[] args) {
-        new Cafe();
+        Cafe myCafe = new Cafe("Campus Cafe", "1 Chapin Way",1);
+        System.out.println("You have built a cafe: ☕");
+        System.out.println(myCafe);
+        myCafe.sellCoffee(101,2,3);
     }
     
 }
--- House.java
+++ House.java
@@ -1,12 +1,48 @@
+import java.util.ArrayList;
 /* This is a stub for the House class */
-public class House {
+public class House extends Building {
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
-  }
+    private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+    private boolean hasDiningRoom;
 
-  public static void main(String[] args) {
-    new House();
-  }
+    public House(String name,String address,int nFloors) {
+      super(name, address, nFloors);
+      this.residents = new ArrayList<String>();
+      this.hasDiningRoom = true;
+      // how can we construct eithe true/false? do we add a parameter?
+    }
+
+    public boolean hasDiningRoom(){
+      return this.hasDiningRoom;
+    }
+    public boolean nResidents(){
+      if(this.residents.size()>0){
+        return true;
+      }else{
+        return false;
+      }
+    }
+
+    public void moveIn(String name){
+      this.residents.add(name);
+    }
+
+    public String moveOut(String name){
+      this.residents.remove(name);
+      return name;
+    } // return the name of the person who moved out  
+
+    public boolean isResident(String person){
+      return this.residents.contains(person);
+    }
+    public static void main(String[] args) {
+      System.out.println("You have built a house: 🏠");
+      House myHouse = new House("Chapin","3 Chapin Way",4);
+      System.out.println(myHouse);
+      myHouse.moveIn("Vivian");
+      System.out.println(myHouse.isResident("Vivian"));
+      myHouse.moveOut("Vivian");
+      System.out.println(myHouse.isResident("Vivian"));
+    }
 
 }
